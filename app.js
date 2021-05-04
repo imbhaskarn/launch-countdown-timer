bbd52a89f800eb9f184c97de574579fe069f37bb
Bhaskar <imbhaskaran@gmail.com>
2021-05-04 12:10:44 +0530

    git commit

---
+++ app.js
@@ -0,0 +1,7 @@
+setInterval( () =>{
+    var date = new Date()
+    let minutes = date.getMinutes()
+    minutes = document.getElementById('minutes').innerHTML
+    let seconds = date.getSeconds()
+    seconds = document.getElementById('seconds').innerHTML
+})
